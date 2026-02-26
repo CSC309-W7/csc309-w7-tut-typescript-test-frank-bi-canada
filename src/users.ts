17bab243c801da7a197d3d117fa760bdd3031646
Frank Bi <frank.bi@mail.utoronto.ca>
2026-02-25 20:48:49 -0500

    getUsersData converts invalid types, does not check valid conversion

--- src/users.ts
+++ src/users.ts
@@ -7,7 +7,13 @@ export const apiResponse: unknown = [
 ];
 
 export function getUsersData(): User[] {
-  return apiResponse as User[]; // intentionally unsafe
+  if (!Array.isArray(apiResponse)) { return []; }
+  apiResponse.forEach(user => {
+    // TODO: Still need to check if user.property is undefined or can be converted
+    if (typeof user.name !== 'string') user.age = String(user.name);
+    if (typeof user.age !== 'number') user.age = Number(user.age);
+  });
+  return apiResponse;
 }
 
 export function formatAges(users: User[]): string[] {
